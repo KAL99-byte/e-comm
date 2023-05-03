205aa7c75b14fe056dda0816af23c16a277a8f55
Kalasa99 <danielkalasa99@gmail.com>
2023-05-03 18:10:40 +0300

    offer changes

--- views/site/offer8.php
+++ views/site/offer8.php
@@ -1,34 +1,27 @@
     <?php
 use yii\helpers\html;
+use app\models\Homephoto;
 
-
+$offers =Homephoto::find()->where(['category'=>'special_offer'])->all();
 ?>
     <!-- Offer Start -->
     <div class="container-fluid pt-5 pb-3">
         <div class="row px-xl-5">
+        <?php foreach ($offers as $offer): ?>
             <div class="col-md-6">
+         
                 <div class="product-offer mb-30" style="height: 300px;">
                   
-                    <?= Html::img('@web/img/offer-1.jpg', ['class' => 'img-fluid', 'alt' => '']) ?>
+                    <?= Html::img('@web/uploads/imgs/'.$offer->photo, ['class' => 'img-fluid', 'alt' => '']) ?>
                     <div class="offer-text">
-                        <h6 class="text-white text-uppercase">Save 20%</h6>
-                        <h3 class="text-white mb-3">Special Offer</h3>
+                        <h6 class="text-white text-uppercase"><?= $offer->description?></h6>
+                        <h3 class="text-white mb-3"><?= $offer->title?></h3>
                         <a href="" class="btn btn-primary">Shop Now</a>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6">
-                <div class="product-offer mb-30" style="height: 300px;">
-                 
-
-                    <?= Html::img('@web/img/offer-2.jpg', ['class' => 'img-fluid', 'alt' => '']) ?>
-                    <div class="offer-text">
-                        <h6 class="text-white text-uppercase">Save 20%</h6>
-                        <h3 class="text-white mb-3">Special Offer</h3>
-                        <a href="" class="btn btn-primary">Shop Now</a>
-                    </div>
-                </div>
-            </div>
         </div>
+            <?php endforeach ?>
+        </div>
+     
     </div>
     <!-- Offer End -->
